895729cb9056d7ba24dd76071cfeeb46873825ad
Tan, Eugene Qi Yuan <eugene.tan@juliusbaer.com>
2025-03-27 20:47:40 +0800

    Initial commit

---
+++ src/main/java/com/sgmonsta/bots/sgpokebot/SgPokebotApplication.java
@@ -0,0 +1,13 @@
+package com.sgmonsta.bots.sgpokebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgPokebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgPokebotApplication.class, args);
+	}
+
+}
